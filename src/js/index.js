1159f06a8d12e854f3c7843e536167c8d371d879
Andrés Clúa <andresclua@gmail.com>
2024-12-27 21:47:48 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,7 @@ import {isEmail} from './utils/isEmail';
 import {isString} from './utils/isString';
 import {isSelect} from './utils/isSelect';
 import {isCheckbox} from './utils/isCheckbox';
+import {isRadio} from './utils/isRadio';
+import { isFile } from './utils/isFile';
 
-export {isEmail, isNumber,isString, isSelect,isCheckbox };
+export {isEmail, isNumber,isString, isSelect,isCheckbox,isRadio,isFile };
