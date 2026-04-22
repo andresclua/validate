c00ce15aa4f0d8634ed3fea6130a0ecd6cff8d72
Andrés Clúa <andres@terrahq.com>
2026-04-22 23:00:46 +0200

    feat: add demo site vite config and npm scripts

---
+++ vite.config.demo.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import glob from 'fast-glob'
+
+const inputs = Object.fromEntries(
+  glob.sync('demo-src/**/*.html').map(f => [
+    f.replace('demo-src/', '').replace('.html', ''),
+    resolve(__dirname, f)
+  ])
+)
+
+export default defineConfig({
+  root: 'demo-src',
+  resolve: {
+    alias: {
+      '@andresclua/validate': resolve(__dirname, 'dist/validate.es.js'),
+    }
+  },
+  build: {
+    outDir: resolve(__dirname, 'demo'),
+    emptyOutDir: true,
+    rollupOptions: { input: inputs }
+  }
+})
