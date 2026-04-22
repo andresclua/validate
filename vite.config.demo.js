33adfcd95c49a256ecbbc61b62d6621ef19993a8
Andrés Clúa <andres@terrahq.com>
2026-04-22 23:06:32 +0200

    fix: vite.config.demo.js style + regex + add /demo to gitignore

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -3,22 +3,22 @@ import { defineConfig } from 'vite'
 import glob from 'fast-glob'
 
 const inputs = Object.fromEntries(
-  glob.sync('demo-src/**/*.html').map(f => [
-    f.replace('demo-src/', '').replace('.html', ''),
-    resolve(__dirname, f)
-  ])
+    glob.sync('demo-src/**/*.html').map(f => [
+        f.replace('demo-src/', '').replace(/\.html$/, ''),
+        resolve(__dirname, f)
+    ])
 )
 
 export default defineConfig({
-  root: 'demo-src',
-  resolve: {
-    alias: {
-      '@andresclua/validate': resolve(__dirname, 'dist/validate.es.js'),
+    root: 'demo-src',
+    resolve: {
+        alias: {
+            '@andresclua/validate': resolve(__dirname, 'dist/validate.es.js'),
+        }
+    },
+    build: {
+        outDir: resolve(__dirname, 'demo'),
+        emptyOutDir: true,
+        rollupOptions: { input: inputs }
     }
-  },
-  build: {
-    outDir: resolve(__dirname, 'demo'),
-    emptyOutDir: true,
-    rollupOptions: { input: inputs }
-  }
-})
+});
